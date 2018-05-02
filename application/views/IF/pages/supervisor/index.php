fb90689b6f20112394926a9035f218d24bc553db
Prem Mann <premmannpnc@gmail.com>
2018-05-02 08:47:41 +0700

    [IF0022] Add some file to IF folder refs IF0022

---
+++ application/views/IF/pages/supervisor/index.php
@@ -0,0 +1,143 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<h2 class="text-center text-primary">List of supervisors</h2>
+		</div>
+		<table id="example" class="table table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Company Name</th>
+                <th>Supervisor Name</th>
+                
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Cloude Net</td>
+                <td>Rith NHEL</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Coding Get</td>
+                <td>Rady Y</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Khalibri</td>
+                 <td>Rady</td>
+               
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>EZECOM</td>
+                <td>Kea LOHN</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Online</td>
+                <td>Sopheak</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Eintelego</td>
+                <td>Pisey</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>RCI</td>
+                <td>Rith NHEL</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Res Up</td>
+                 <td>Chanak CHON</td>
+               
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>Camsolution</td>
+                <td>Rady Y</td>
+                
+                <td>
+               
+                <a href="<?php echo base_url() ?>/Welcome_IF/viewSupervisor"><i class="mdi mdi-eye text-success" style="font-size: 24px;" ></i></a>
+                <a href="<?php echo base_url() ?>/Welcome_IF/editSupervisor"><i class="mdi mdi-pencil text-success" style="font-size: 24px;" ></i></a>
+                <a href="" onclick="return confirm('Are you sur want to delete?')"><i class="mdi mdi-delete text-danger" style="font-size: 24px;"></i></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+	</div>
+    <a href="<?php echo base_url(); ?>Welcome_IF/createSupervisor">
+		<button class="btn btn-primary"><i class="mdi mdi-account-plus" style="font-size: 20px;"></i>New Supervisor</button>
+	</a>
+
+</div>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<script>
+	$(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>

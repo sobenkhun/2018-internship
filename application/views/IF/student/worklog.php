3dd92a77d26b1cabb39dcf8e95354adc45226dd0
Soben.KHUN <Soben.KHUN@PNC.LAN>
2018-05-03 08:51:18 +0700

    [SIF005] I update on the dashboard weekly work log refs SIF005

--- application/views/IF/student/worklog.php
+++ application/views/IF/student/worklog.php
@@ -1,115 +1,128 @@
 <!-- body -->
-<!-- <div class="container"> -->
-	<div class="row-fluid">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<h3 class="text-primary text-center"><strong>Weekly work-log report</strong></h3>
-			<br>
-			<table class="table table-striped table-bordered text-center" style="width:100%">
-				<thead class="bg-info text-white">
-					<tr>
-						<th>Date</th>
-						<th>Work Activities</th>
-						<th>What did you learn?</th>
-						<th>Difficulties/Issues</th>
-						<th>Solutions</th>
-						<th>To Do (Plan)</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<!-- <td>
-						    <input class="form-control" type="date" value="DD/MM/YYY"> 
-						</td> -->
-						<td>
-							<div class="input-group date form_datetime" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="dtp_input1">
-								<input class="form-control" size="16" type="text" value="" readonly>
-								<span class="input-group-addon">
-									<span class="glyphicon glyphicon-remove">
-										<!-- <i  class="mdi mdi-email"></i> -->
-									</span>
-								</span>
-								<span class="input-group-addon">
-									<span class="glyphicon glyphicon-th">
-										<!-- <i  class="mdi mdi-email"></i> -->
-									</span>
-								</span>
-							</div>
-							<input type="hidden" id="dtp_input1" value="" /><br/>
-						</td>
-						<td>
-							<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-							<td>
-								<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-								<td>
-									<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-									<td>
-										<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-										<td>
-											<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-										</tr>
-										<tr>
-											<td>
-												<input class="form-control" type="date" value="DD/MM/YYY"> 
-											</td>
-											<td>
-												<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-												<td>
-													<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-													<td>
-														<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-														<td>
-															<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-															<td>
-																<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-															</tr>
-															<tr>
-																<td>
-																	<input class="form-control" type="date" value="DD/MM/YYY"> 
-																</td>
-																<td>
-																	<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																	<td>
-																		<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																		<td>
-																			<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																			<td>
-																				<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																				<td>
-																					<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																				</tr>
-																				<tr>
-																					<td>
-																						<input class="form-control" type="date" value="DD/MM/YYY"> 
-																					</td>
-																					<td>
-																						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																						<td>
-																							<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																							<td>
-																								<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																								<td>
-																									<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																									<td>
-																										<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																									</tr>
-																									<tr>
-																										<td>
-																											<input class="form-control" type="date" value="DD/MM/YYY"> 
-																										</td>
-																										<td>
-																											<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																											<td>
-																												<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																												<td>
-																													<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																													<td>
-																														<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																														<td>
-																															<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea></td>
-																														</tr>
-																													</tbody>
-																												</table>
-																											</div>
-																										</div>
-																										<!-- </div> -->
-																										<!-- //body -->
+<div class="row-fluid">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<h3 class="text-primary text-center"><strong>Weekly work-log report</strong></h3>
+		<br>
+		<table class="table table-bordered table-responsive w-auto" style="width:100%">
+			<thead class="bg-info text-white">
+				<tr>
+					<th>Date</th>
+					<th>Work Activities</th>
+					<th>What did you learn?</th>
+					<th>Difficulties/Issues</th>
+					<th>Solutions</th>
+					<th>To Do (Plan)</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>
+						<input class="form-control" type="date" value="DD/MM/YYY">
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>	
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>		
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>			
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>				
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>					
+				</tr>
+				<tr>
+					<td>
+						<input class="form-control" type="date" value="DD/MM/YYY"> 
+					</td>						
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>						
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>							
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>								
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>									
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>											
+				</tr>
+				<tr>
+					<td>
+						<input class="form-control" type="date" value="DD/MM/YYY"> 
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>											
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>														
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>															
+				</tr>
+				<tr>
+					<td>
+						<input class="form-control" type="date" value="DD/MM/YYY"> 
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input class="form-control" type="date" value="DD/MM/YYY"> 
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+					<td>
+						<textarea class="form-group" name="" id="" cols="25" rows="3" placeholder="..."></textarea>
+					</td>
+				</tr>
+				<tr class="table-dark text-dark text-center">
+					<td>text 1</td> 	
+					<td>text 2</td> 	
+					<td>text 3</td> 	
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+	<!-- //body -->

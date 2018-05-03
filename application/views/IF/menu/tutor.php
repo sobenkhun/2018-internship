bd04dbe33f14b988d4ed8eb3073b2590944b043a
Ban-Touch <touch.ban9@gmail.com>
2018-05-03 15:17:06 +0700

    [TIF0018]Continue doing on tutor dashboard

--- application/views/IF/menu/tutor.php
+++ application/views/IF/menu/tutor.php
@@ -1,3 +1,5 @@
+
+
 <nav class=" navbar navbar-expand-lg navbar-light  color">
   <a class="navbar-brand" href="<?php echo base_url();?>tutorDas/index">
       <img class="logo" src="<?php echo base_url();?>assets/images/logoL.png" alt="Logo" style="width:150px;">
@@ -6,50 +8,45 @@
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class=" collapse navbar-collapse" id="navbarSupportedContent">
+  <div class=" collapse navbar-collapse" id="navbarSupportedContent" style="font-family: sans-serif;">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active ">
-        <a class="nav-link" href="#">
-        <b class="colorTextMenu">  Company</b></a>
+        <a class="nav-link" href="<?php echo base_url();?>tutorDas/companyDas"><b class="colorTextMenu">  Company</b></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url();?>tutorDas/supervisorDas"><b class="colorTextMenu">  Supervisor</b></a>
       </li>
       
       <li class="nav-item active ">
-        <a class="nav-link" href="<?php echo base_url();?>tutorDas/supervisorDas">
-        <b class="colorTextMenu">  Supervisor</b></a>
+        <a class="nav-link" href="<?php echo base_url();?>tutorDas/studentDas"><b class="colorTextMenu">  Student</b></a>
+      </li><li class="nav-item active ">
+        <a class="nav-link" href="<?php echo base_url();?>tutorDas/listCommStuDas"><b class="colorTextMenu">  Comment</b></a>
       </li>
       <li class="nav-item active ">
-        <a class="nav-link" href="<?php echo base_url();?>tutorDas/studentDas">
-        <b class="colorTextMenu">  Student</b></a>
-      </li>
-      <li class="nav-item active ">
-        <a class="nav-link" href="<?php echo base_url();?>tutorDas/listCommStuDas">
-        <b class="colorTextMenu"> Comment</b></a>
-      </li>
-      <li class="nav-item active ">
-        <a class="nav-link" href="<?php echo base_url();?>tutorDas/calendar">
-        <b class="colorTextMenu">Calendar   <i class="mdi mdi-book" style="font-size:12px; margin-bottom: -9px;"></i></b></a>
+        <a class="nav-link" href="<?php echo base_url();?>tutorDas/calendar"><b class="colorTextMenu">Calendar   <i class="mdi mdi-book" style="font-size:12px; margin-top: 50px;"></i></b></a>
+        <!-- <img src="note.png" style="width: 3%;" alt=""> -->
       </li>
     </ul>
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a href="#" class="nav-link active">
-          <i  style="font-size: 20px; margin-right: 20px;" class="mdi mdi-email"> </i>          
-        </a>
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link" href="#">       
-          <b class="colorTextMenu">  
-            <span> Username </span><i style="font-size: 20px;" class="mdi mdi-account-circle"></i> 
-          </b>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <b class="colorTextMenu"> 
-            <span> Logout </span> <i style="font-size: 20px;" class="mdi mdi-login"></i> 
-          </b>
-        </a>
-      </li>
-    </ul>
+         <li class="nav-item">
+           <a href="#" class="nav-link active">
+             <i  style="font-size: 20px; margin-right: 20px;" class="mdi mdi-email"> </i>          
+           </a>
+         </li>
+         <li class="nav-item ">
+           <a class="nav-link" href="#">       
+             <b class="colorTextMenu">  
+               <span> Username </span><i style="font-size: 20px;" class="mdi mdi-account-circle"></i> 
+             </b>
+           </a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="<?php echo base_url();?>Welcome_IF">
+             <b class="colorTextMenu"> 
+               <span> Logout </span> <i style="font-size: 20px;" class="mdi mdi-login"></i> 
+             </b>
+           </a>
+         </li>
+       </ul>
   </div>
 </nav><br><br>

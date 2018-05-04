4a43b7fc5628082980f65f0300e92eed05d016bd
bunthean <bunthean.mov2727@gmail.com>
2018-05-04 09:30:48 +0700

    Test on student home page

---
+++ application/views/IF/student/index.php
@@ -0,0 +1,52 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 alert alert-primary text-center" style="background: #7dbcf9; font-family: sans-serif;">
+      <h2 class="text-center text-dark">Welcome To PNC Internship Follow up</h2>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-6"  >
+          <div class="">
+            <div class="row"> 
+                <!--  test commit-->
+              <div class="col-md-12 ">
+                <div class="card text-center text-white">
+                  <div class="card-header" style="background-color: #FFD740 ;">
+                    <i class="mdi mdi-comment-text" style="font-size:70px;"></i><br><br>
+                    <h4 style="font-family:  sans-serif;">View Your Comments</h4>
+                  </div>
+                  <div class="card-body">
+
+                    <a style="background-color: #31b5eb ; font-family: verdana" href="<?php echo base_url();?>cStudent/comment" class="btn btn-primary">Go Comment</a>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6" >
+          <div class="row">
+            <div class="col-md-12 "> 
+              <div class="card text-center text-white">
+                <div class="card-header" style="background-color: #4e95cb ;">
+                  <i class="mdi mdi-book-open-page-variant " style="font-size:70px;"></i><br><br>
+                  <h4 style="font-family:  sans-serif">Edit On Your Worklog</h4>
+                </div>
+                <div class="card-body">
+                  <a style="background-color: #31b5eb ; font-family: verdana" href="<?php echo base_url();?>cStudent/worklog" class="btn btn-primary">Go Worklog</a>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+

dc8eba3e613ffda420af77cd17d6a0e2cd93f2b9
Soben.KHUN <Soben.KHUN@PNC.LAN>
2018-05-07 09:38:17 +0700

    [SIF003] I update on user student refs SIF003

--- application/views/IF/student/comment.php
+++ application/views/IF/student/comment.php
@@ -1,21 +1,14 @@
 
 <div class="container con-body">
-	<div class="row">
-		<a href="<?php echo base_url();?>Welcome_IF/student">
-			<button type="button" class="btn btn-primary">
-				<i class="mdi mdi-chevron-left"></i>Back
-			</button>
-		</a>
-	</div>	 
 	<h3 class="text-primary text-center"><strong>Comment of student</strong></h3>
 	<div class="row"><br><br>
-		<!-- <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div> -->
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<table id="studentList" class="table table-striped table-bordered bg-white" style="width:100%">
 				<thead class="text-center">
 					<tr>
+						<th>ID</th>
 						<th>Date</th>
-						<th>Student</th>
+						<th>Who comment?</th>
 						<th>Comment</th>
 					</tr>
 				</thead>
@@ -23,39 +16,39 @@
 					<tr>
 						<td class="text-center">1</td>
 						<td>1 may 2018</td>
+						<td>Dara</td>
 						<td class="text-center"> Devit is always let in meeting...</td>
 					</tr>
 					<tr>
 						<td class="text-center">1</td>
 						<td>14 may 2018</td>
+						<td>Admin</td>
 						<td class="text-center"> Devit should buy a clock for...</td>
 					</tr>
 					<tr>
 						<td class="text-center">1</td>
 						<td>22 may 2018</td>
+						<td>Socheat</td>
 						<td class="text-center"> Devit is has always good at...</td>
 					</tr>
 					<tr>
 						<td class="text-center">1</td>
 						<td>1 may 2018</td>
+						<td>Thaina</td>
 						<td class="text-center"> Devit is geting better and...</td>
 					</tr>
 
 				</tbody>
 			</table>
 		</div>
-		<!-- <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div> -->
 	</div>
-	<div class="row">
-		<!-- <div class="col-md-"></div> -->
-		<!-- <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div> -->
+	<!-- <div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-right">
-			<a href="<?php echo base_url();?>Welcome_IF/addStudent">
+			<a href="<?php echo base_url();?>cStudent/addComment">
 				<button class="btn btn-primary float-left">
 					<i class="mdi mdi-account-plus text-white" style="font-size:20px;"></i>Add Comment
 				</button>
 			</a>
 		</div>
-		<!-- <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8"></div> -->
-	</div>
+	</div> -->
 </div>

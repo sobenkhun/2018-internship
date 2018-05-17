f720c0ea1829fdd3ae580e47c50e566edf5448a7
chea devit <devit.chea1998@gmail.com>
2018-05-17 08:00:55 +0700

    correct on view company on tutor dashboar that it have error

--- application/views/tutorDashboard/supervisor/index.php
+++ application/views/tutorDashboard/supervisor/index.php
@@ -24,7 +24,7 @@
                 <td><?php echo $supervisor['supervisorName']; ?></td>
                 <td class="text-center">
 
-                    <a href="<?php echo base_url() ?>tutorDas/viewSupervisorDas?&id=<?php echo $supervisor['id'];?>"><i class="mdi mdi-eye mdi-24px text-info" data-toggle ="tooltip" title="View "></i></a>
+                    <a href="<?php echo base_url() ?>tutorDas/getSupDataDetail?&id=<?php echo $supervisor['id'];?>"><i class="mdi mdi-eye mdi-24px text-info" data-toggle ="tooltip" title="View "></i></a>
                     <a href="<?php echo base_url() ?>tutorDas/questionnair"><i class="mdi mdi-book-minus mdi-24px text-dark" data-toggle ="tooltip" title=" Questionniare"></i></a>
                     
                 </td>

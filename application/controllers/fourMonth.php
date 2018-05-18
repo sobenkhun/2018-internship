1892a39f486d7bf69ca511b170bd557512f0d14f
Ban-Touch <touch.ban9@gmail.com>
2018-05-18 13:49:19 +0700

    resolve conflic

--- application/controllers/fourMonth.php
+++ application/controllers/fourMonth.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php 
 	class fourMonth extends CI_controller {
 		public function firstWeek() 
@@ -31,38 +31,5 @@
 		}
 	}
 
-=======
-<?php 
-	class fourMonth extends CI_controller {
-		public function firstWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthFour/weekOne');
-			$this->load->view('templates/footer.php');
-		}
-		public function secounWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthFour/weekTwo');
-			$this->load->view('templates/footer.php');
-		}
-		public function thirdWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthFour/weekThree');
-			$this->load->view('templates/footer.php');
-		}
-		public function fourWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthFour/weekFour');
-			$this->load->view('templates/footer.php');
-		}
-	}
 
->>>>>>> 4bbcc2b52e1856b15b1b2257c70dc4d8752616cc
  ?>
--- application/controllers/secounMonth.php
+++ application/controllers/secounMonth.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php 
 	class secounMonth extends CI_controller {
 		public function firstWeek() 
@@ -31,38 +31,5 @@
 		}
 	}
 
-=======
-<?php 
-	class secounMonth extends CI_controller {
-		public function firstWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthTwo/weekOne');
-			$this->load->view('templates/footer.php');
-		}
-		public function secounWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthTwo/weekTwo');
-			$this->load->view('templates/footer.php');
-		}
-		public function thirdWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthTwo/weekThree');
-			$this->load->view('templates/footer.php');
-		}
-		public function fourWeek() 
-		{
-			$this->load->view('templates/header.php');
-			$this->load->view('menu/studentMenu.php');
-			$this->load->view('student/monthReport/monthTwo/weekFour');
-			$this->load->view('templates/footer.php');
-		}
-	}
 
->>>>>>> 4bbcc2b52e1856b15b1b2257c70dc4d8752616cc
  ?>

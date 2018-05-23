318901f9f81b0c4461efaf34091a89debe2c6616
chea devit <devit.chea1998@gmail.com>
2018-05-23 20:13:25 +0700

    DIF0051 delete code on menu

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,4 +25,20 @@ class Welcome extends CI_Controller {
 		$this->load->view('pages/index', $data);
 		$this->load->view('IF/template/footer', $data);
 	}	
+
+	public function sendMail(){
+	  $this->load->library('email');
+	  $this->load->model('users_model');
+	  
+	  $this->email->from('pnc.temporary.vc2018@passerellesnumeriques.org', 'Booking gig');
+	  $this->email->to('devit.chea@student.passerellesnumeriques.org', "devit chea");
+	  $this->email->subject(' R');
+	  $this->email->message('Dear ');
+	  if ($this->email->send()) {
+	   	echo "successfully";
+	  }else{
+	   echo $this->email->print_debugger();
+	  }
+	  
+	 }
 }
